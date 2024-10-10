187ea86e13f05419d2ed663ebe3e6bf1e251ba3d
Shahzain333 <shahzainkhannaizi1234@gmail.com>
2024-10-10 15:19:05 +0500

    Add via file uploaded

---
+++ 02counterProject/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  //let counter = 0
+
+  const addValue = () => {
+    // console.log("Value Added", Math.random());
+    //counter = counter + 1
+    //console.log("Value Added", counter);
+    setCounter(counter + 1)
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+
+      <h1>Chai Aur React</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick = {addValue}>
+        Add value
+      </button>
+      
+      <br />
+      
+      <button onClick = {removeValue}>
+        Remove Value
+      </button>
+
+    </>
+  )
+}
+
+export default App
